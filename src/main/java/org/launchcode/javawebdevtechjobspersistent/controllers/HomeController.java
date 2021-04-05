882f29ef81b1537850684d3d1de27c234e6094c1
Howstressful <flameblood@live.com>
2021-04-05 11:21:55 -0500

    test

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,6 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -15,11 +22,20 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+@Autowired
+public EmployerRepository employerRepository;
+
+@Autowired
+public JobRepository jobRepository;
+
+@Autowired
+public SkillRepository skillRepository;
+
     @RequestMapping("")
     public String index(Model model) {
 
         model.addAttribute("title", "My Jobs");
-
+        //model.addAttribute("jobs",jobRepository.findAll());
         return "index";
     }
 
@@ -27,6 +43,11 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        model.addAttribute("skills",skillRepository.findAll());
+        Iterable<Employer> employers = employerRepository.findAll();
+        model.addAttribute("employers",employers);
+
+
         return "add";
     }
 
@@ -34,12 +55,19 @@ public class HomeController {
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
                                        Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
 
-        if (errors.hasErrors()) {
+Employer employer = employerRepository.findById(employerId).orElse(new Employer());
+List<Skill> skill = (List<Skill>) skillRepository.findAllById(skills);
+
+if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             return "add";
         }
 
-        return "redirect:";
+        newJob.setSkills(skill);
+        newJob.setEmployer(employer);
+        jobRepository.save(newJob);
+return "view";
+
     }
 
     @GetMapping("view/{jobId}")
@@ -49,4 +77,8 @@ public class HomeController {
     }
 
 
+
 }
+
+
+
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,52 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+@Entity
 public class Employer extends AbstractEntity {
 
+@NotBlank(message = "gonna need this to not be blank")
+private String location;
 
+@OneToMany
+@JoinColumn
+private List<Job> jobs = new ArrayList<>();
+/*
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+    @Override
+    public int getId() {
+        return super.getId();
+    }
+
+    @NotBlank(message = "That's a negative ghost rider")
+    @Size(min = 3, max = 80, message = "Name must be between 3 and 80 characters")
+
+*/
+
+public Employer(){}
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+    public void setLocation(String location){
+        this.location = location;
+    }
 }
